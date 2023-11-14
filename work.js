b49ae60ee291b39cbddbbf2b6957eee2e419fec2
weiting93116 <weiting93116@gmail.com>
2023-11-15 02:14:58 +0800

    第五次修改

--- work.js
+++ work.js
@@ -10,3 +10,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 		});
 	});
 });
+
+
+function isElementInViewport(el) {
+	const rect = el.getBoundingClientRect();
+	return (
+	  rect.top >= 0 &&
+	  rect.left >= 0 &&
+	  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+	  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+  }
+  $(function(){
+    // 
+    var duration = 300;
+
+    // aside ----------------------------------------
+    var $aside = $('.page-main > aside');
+    var $asidButton = $aside.find('.qq')
+        .on('click', function(){
+            $aside.toggleClass('open');
+            if($aside.hasClass('open')){
+                $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+                $asidButton.find('img').attr('src', 'https://c2.staticflickr.com/6/5635/31065147822_9b6e31ab5f_o.png');
+            }else{
+                $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+                $asidButton.find('img').attr('src', 'https://c2.staticflickr.com/6/5555/31208490685_5c55f2f28f_o.png');
+            };
+        });
+
+});
