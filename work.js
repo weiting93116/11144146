aefeb8bcff8f380256c93cccf9ec0ed0997dc791
weiting93116 <weiting93116@gmail.com>
2023-11-15 16:10:05 +0800

    第七次更新

--- work.js
+++ work.js
@@ -21,22 +21,3 @@ function isElementInViewport(el) {
 	  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
   }
-  $(function(){
-    // 
-    var duration = 300;
-
-    // aside ----------------------------------------
-    var $aside = $('.page-main > aside');
-    var $asidButton = $aside.find('.qq')
-        .on('click', function(){
-            $aside.toggleClass('open');
-            if($aside.hasClass('open')){
-                $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
-                $asidButton.find('img').attr('src', 'https://c2.staticflickr.com/6/5635/31065147822_9b6e31ab5f_o.png');
-            }else{
-                $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
-                $asidButton.find('img').attr('src', 'https://c2.staticflickr.com/6/5555/31208490685_5c55f2f28f_o.png');
-            };
-        });
-
-});
